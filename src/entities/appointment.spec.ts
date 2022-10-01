f51e006116294fa6cdfc919a7790012f3d717c14
lucaspires-source <pires.lucas94@gmail.com>
2022-10-01 15:51:10 -0300

    implementing new util

--- src/entities/appointment.spec.ts
+++ src/entities/appointment.spec.ts
@@ -1,11 +1,11 @@
 import {expect, test} from 'vitest'
+import { getFutureDate } from '../tests/utils/get-future-date'
 import { Appointment } from './appointment'
 
 test('create a new appointment', () => {
-    const startsAt = new Date()
-    const endsAt = new Date()
-    startsAt.setDate(startsAt.getDate() + 1)
-    endsAt.setDate(endsAt.getDate() + 2)
+    const startsAt =  getFutureDate('2022-08-10')
+    const endsAt = getFutureDate('2022-08-11')
+
     const appointment = new Appointment({
         customer:'john doe',
         startsAt,
--- src/use-cases/create-appointment.spec.ts
+++ src/use-cases/create-appointment.spec.ts
@@ -1,13 +1,13 @@
 import {describe,it, expect} from 'vitest'
 import { Appointment } from '../entities/appointment'
 import { CreateAppointment } from './create-appointment'
+import { getFutureDate } from '../tests/utils/get-future-date'
 describe('Create Appointment',() =>{
     it('Should be able to create a new Appointment',() =>{
         const createAppointment = new CreateAppointment()
-        const startsAt = new Date()
-        const endsAt = new Date()
-        startsAt.setDate(startsAt.getDate() + 1)
-        endsAt.setDate(endsAt.getDate() + 2)
+        const startsAt =  getFutureDate('2022-08-10')
+        const endsAt = getFutureDate('2022-08-11')
+    
         expect(createAppointment.execute({
             customer:'John Doe',
             startsAt,
